fa682514a38afeb4e7a3151cee5e7fa1b24712a9
Vasant Saraswat <vasant.saraswat@gmail.com>
2022-05-17 21:37:17 +0530

    Assignment of making article app is completed

---
+++ block-BRaaao/code/basic/src/components/App.js
@@ -0,0 +1,17 @@
+// import data from '../data';
+import '../styles/app.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
