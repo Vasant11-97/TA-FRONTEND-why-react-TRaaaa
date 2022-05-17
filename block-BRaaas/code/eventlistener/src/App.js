29236cf3759d087df61f369373dce578581deddc
Vasant Saraswat <vasant.saraswat@gmail.com>
2022-05-17 22:24:35 +0530

    Assingment of addding event listener is completed

---
+++ block-BRaaas/code/eventlistener/src/App.js
@@ -0,0 +1,18 @@
+import Hero from './components/Hero';
+import Greet from './components/Greet';
+import Help from './components/Help';
+import Fruit from './components/Fruit';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Help />
+      <Greet />
+      <Greet />
+      <Fruit />
+    </div>
+  );
+}
+
+export default App;
